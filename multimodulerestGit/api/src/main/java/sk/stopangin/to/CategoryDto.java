6404a3eb12e171d18d6f1c55c8506903593e33ec
stopAngin <mozdev973@gmail.com>
2015-07-12 09:07:03 +0200

    test

--- multimodulerestGit/api/src/main/java/sk/stopangin/to/CategoryDto.java
+++ multimodulerestGit/api/src/main/java/sk/stopangin/to/CategoryDto.java
@@ -2,9 +2,7 @@ package sk.stopangin.to;
 
 import java.io.Serializable;
 
-/**
- * Created by myPC on 24. 5. 2015.
- */
+
 public class CategoryDto implements Serializable{
     private String name;
 
